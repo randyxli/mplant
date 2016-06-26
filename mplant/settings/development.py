7b44ba05d834b6ee0aa076ee3dd9a69c8260c4c0
Randy Li <randyxli@gmail.com>
2016-06-26 09:55:57 -0700

    Set up Travis CI

--- mplant/settings/development.py
+++ mplant/settings/development.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mplant',
         'USER': 'mplant',
-        'PASSWORD': 'uP*eX4ABF4',
+        'PASSWORD': 'mplant',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
