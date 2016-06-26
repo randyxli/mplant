4d279eea43e6281b62aebb5a88af6e4bffa83d55
Randy Li <randyxli@gmail.com>
2016-06-25 18:29:14 -0700

    Configure mplant to be deployable to Heroku   * Add requirements.txt, runtime.txt, and Procfile   * Create mplant.settings module with different settings for development and production environments.

--- mplant/settings/base.py
+++ mplant/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '((+0l&whwbxjp%og#3dx7b884#7i8#*=2f8@y8l5jbz)*uedc2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -71,21 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mplant.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mplant',
-        'USER': 'mplant',
-        'PASSWORD': 'uP*eX4ABF4',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -117,9 +101,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
---
+++ mplant/settings/development.py
@@ -0,0 +1,16 @@
+from mplant.settings.base import *
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mplant',
+        'USER': 'mplant',
+        'PASSWORD': 'uP*eX4ABF4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
