06cd8667e25553a4863fa3bdf028f223488f938b
Randy Li <randyxli@gmail.com>
2016-07-02 23:59:44 -0700

    Create initial models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rent = models.IntegerField()
+    country = models.CharField(max_length=256) # i.e. USA, China
+    administrative_area = models.CharField(max_length=256) # i.e. California, Fujian
+    locality = models.CharField(max_length=1024) # i.e. San Francisco, Fuzhou
+    location = models.CharField(max_length=1024) # ideally contains address or crossroad
+    data = models.CharField(max_length=13172)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    template = models.CharField(max_length=8192)
--- mplant/settings/base.py
+++ mplant/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'listings.apps.ListingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
